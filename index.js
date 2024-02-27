b727daaed73b8778e0bb44c0500dd10d7c31177c
Endryus <endryush@gmail.com>
2024-02-27 12:29:52 -0300

    meio do desenvolvimento, subindo pro github

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import winston from 'winston'
+import { promises as fs} from 'fs'
+import accountsRouter from './routes/accounts.js'
+
+const { readFile, writeFile } = fs
+// global json file to write and read items for learning purposes
+global.filename = 'accounts.json'
+const { combine, timestamp, label, printf } = winston.format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}]  ${level}: ${message}`
+})
+global.logger = winston.createLogger({
+  level: 'silly',
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({ filename: 'bank-api-logger.log' })
+  ],
+  format: combine(
+    label({ label: 'my-bank-api' }),
+    timestamp(),
+    myFormat
+  )
+})
+const app = express()
+app.use(express.json())
+
+
+// routes
+app.use('/account', accountsRouter )
+
+app.listen(3000, async ()=> {
+  try {
+    await readFile(global.filename)
+    logger.info('Server is running on port 3000')
+
+  } catch (error) {
+    const initialJson = {
+      nextId: 1,
+      accounts: []
+    }
+    await writeFile(global.filename, JSON.stringify(initialJson))
+    logger.info('Server is running on port 3000, initialJson has created')
+  }
+})
---
+++ routes/accounts.js
@@ -0,0 +1,129 @@
+import express from 'express'
+import { promises as fs} from 'fs'
+
+
+const { readFile, writeFile } = fs
+const router = express.Router()
+
+
+router
+  .post('/', async (req, res) => {
+    try {
+      let account = req.body
+
+      if (!account.name || account.balance === null ||  account.balance === undefined) {
+        throw new Error ('campos name e balance são obrigatórios')
+      }
+      const data = JSON.parse(await readFile(global.filename))
+      account = {
+        id:  data.nextId ++,
+        name: account.name,
+        balance: account.balance
+      }
+
+      data.accounts.push(account)
+
+      await writeFile(global.filename, JSON.stringify(data, null, 2))
+
+      logger.info(`POST /account - ${JSON.stringify(account)}`)
+      res.send(account)
+    } catch (error) {
+      logger.error(`${req.method} => ${req.baseUrl} ${error.message}`)
+      res.status(500).send({ error: error.message })
+    }
+  })
+
+  .get('/', async (req, res) => {
+    try {
+      const data = JSON.parse(await readFile(global.filename))
+
+      logger.info(`GET /account`)
+      res.send({ accounts: data.accounts })
+    } catch (error) {
+      logger.error(`${req.method} => ${req.baseUrl} ${error.message}`)
+      res.status(500).send({ error: error.message })
+    }
+  })
+
+  .get('/:id', async (req, res) => {
+    try {
+      const { id } = req.params
+      const data = JSON.parse(await readFile(global.filename)).accounts
+      const currentAccount = data.find(account => account.id === parseInt(id))
+
+      logger.info(`GET /account/:id`)
+
+      res.send(currentAccount)
+    } catch (error) {
+      logger.error(`${req.method} => ${req.baseUrl} ${error.message}`)
+      res.status(500).send({ error: error.message })
+    }
+  })
+
+  .delete('/:id', async (req, res) => {
+    try {
+      const { id } = req.params
+      const data = JSON.parse(await readFile(global.filename))
+      data.accounts = data.accounts.filter(account => account.id !== parseInt(id))
+
+      await writeFile(global.filename, JSON.stringify(data, null, 2))
+      logger.info(`DELETE /account/:id => account deleted = ${id}`)
+      res.end()
+    } catch (error) {
+      logger.error(`${req.method} => ${req.baseUrl} ${error.message}`)
+      res.status(500).send({ error: error.message })
+    }
+  })
+
+  .put('/', async (req, res) => {
+    try {
+      const account = req.body
+      const data = JSON.parse(await readFile(global.filename))
+      console.error('indes', data)
+      const index = data.accounts.findIndex(dataAccount => dataAccount.id === account.id)
+      if (index === -1) throw new Error('account not found')
+      
+      if (!account.name || account.balance === null ||  account.balance === undefined) {
+        throw new Error ('campos name e balance são obrigatórios')
+      }
+      
+
+      data.accounts[index].name = account.name
+      data.accounts[index].balance = account.balance
+
+      await writeFile(global.filename, JSON.stringify(data))
+      
+      logger.info(`PUT /account - ${JSON.stringify(account)}`)
+
+      res.send(account)
+    } catch (error) {
+      logger.error(`${req.method} => ${req.baseUrl} ${error.message}`)
+      res.status(500).send({ error: error.message })
+    }
+  })
+
+  .patch('/updateBalance', async (req, res) => {
+    try {
+      const account = req.body
+
+      const data = JSON.parse(await readFile(global.filename))
+      const index = data.accounts.findIndex(dataAccount => dataAccount.id === account.id)
+
+      if (index === -1) throw new Error('account not found')
+      
+      data.accounts[index].balance = account.balance
+
+      await writeFile(global.filename, JSON.stringify(data))
+
+      logger.info(`PATCH /account/updateBalance - ${JSON.stringify(account)}`)
+      
+      res.send(data.accounts[index])
+    } catch (error) {
+      logger.error(`${req.method} => ${req.baseUrl} ${error.message}`)
+      res.status(500).send({ error: error.message })
+    }
+  })
+
+
+export default router
+
